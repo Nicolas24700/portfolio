f07237295c0a54649cd15c5b0d8e026ba5263646
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2024-11-30 11:04:23 +0100

    animation de changement de couleur + animation d'un bouton

--- script.js
+++ script.js
@@ -10,39 +10,62 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('open');
     });
 
-
-    // Script du bouton switch
-    document.getElementById('toggle').addEventListener('change', function () {
-        const toggleWrapper = document.querySelector('.toggle-wrapper');
+    document.getElementById('toggle').addEventListener('click', function () {
         const selector = document.querySelector('.selector');
         const root = document.documentElement;
         const sun = document.querySelector('.fa-sun');
         const moon = document.querySelector('.fa-moon');
 
+        const swipe = document.getElementById('swipe');
+        const body = document.body;
+
+        swipe.classList.add('current');
+
         if (toggle.checked) {
-            // Affiche l'icone de la lune et cache celle du soleil
             sun.style.display = 'block';
             moon.style.display = 'none';
-
             // Déplace le sélecteur à droite
             selector.style.left = 'calc(100% - 50px)';
-            // Inverse les couleurs en changeant la couleur des variables CSS
-            root.style.setProperty('--main-color', 'white');
-            root.style.setProperty('--second-color', 'black');
-            selector.style.backgroundColor = 'var(--second-color)';
-            toggleWrapper.style.backgroundColor = 'var(--main-color)';
+            setTimeout(() => {
+                // Inverse les couleurs en changeant la couleur des variables CSS
+                root.style.setProperty('--main-color', 'white');
+                root.style.setProperty('--second-color', 'black');
+
+
+                swipe.classList.remove('current');
+            }, 600);
         } else {
             // Affiche l'icone du soleil et cache celle de la lune
             sun.style.display = 'none';
             moon.style.display = 'block';
-            toggleWrapper.style.backgroundColor = 'var(--main-color)';
             // Inverse les couleurs en changeant la couleur des variables CSS
-            root.style.setProperty('--main-color', 'black');
-            root.style.setProperty('--second-color', 'white');
             // Déplace le sélecteur à gauche
             selector.style.left = '10px';
-            selector.style.backgroundColor = 'var(--second-color)';
+
+            setTimeout(() => {
+                // Inverse les couleurs en changeant la couleur des variables CSS
+                root.style.setProperty('--main-color', 'black');
+                root.style.setProperty('--second-color', 'white');
+
+
+                swipe.classList.remove('current');
+
+            }, 600);
         }
+    });
+
+    const contactLink = document.querySelector('.buttons-contact');
+    const arrowIcon = contactLink.querySelector('.fa-arrow-right');
+
+    contactLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Empêche la redirection immédiate
+        arrowIcon.style.transform = 'translateX(-200px)'; // Animation vers la gauche
+
+
+        setTimeout(() => {
+            window.location.href = contactLink.getAttribute('href');
+            arrowIcon.style.transform = 'translateX(0px)';  // Redirection après animation
+        }, 300); // Temps pour l'animation (300ms)
     });
 
 });
