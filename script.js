bf80ff06441274512d8cb46341be3d44f1079cd7
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2024-12-06 11:05:15 +0100

    Ajout de fonctionnalité
    - Sauvergarde de la couleur de fond et du texte dans le localStorage ,pour le mode Jour/Nuit
    - Ajout de la section Parcours
    - Ajout de mon CV

--- script.js
+++ script.js
@@ -10,14 +10,41 @@ document.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('open');
     });
 
-    document.getElementById('toggle').addEventListener('click', function () {
-        const selector = document.querySelector('.selector');
-        const root = document.documentElement;
-        const sun = document.querySelector('.fa-sun');
-        const moon = document.querySelector('.fa-moon');
+    // SCRIPT du toggle qui permet de mettre le mode JOUR / NUIT ========================================================
+
+    const selector = document.querySelector('.selector');
+    const root = document.documentElement;
+    const sun = document.querySelector('.fa-sun');
+    const moon = document.querySelector('.fa-moon');
+    const toggle = document.getElementById('toggle');
+
+    // Récupére les couleurs stockées dans le localStorage
+    const savemaincolor = localStorage.getItem('--main-color');
+    const savesecondcolor = localStorage.getItem('--second-color');
+
+    // Si les deux couleurs sont définies, appliquer les couleurs appropriées
+    if (savemaincolor && savesecondcolor) {
+        root.style.setProperty('--main-color', savemaincolor);
+        root.style.setProperty('--second-color', savesecondcolor);
+        // Si la couleur principale est blanche, affiche l'icone du soleil
+        if (savemaincolor === 'white') {
+            toggle.checked = true;
+            sun.style.display = 'block';
+            moon.style.display = 'none';
+            selector.style.left = 'calc(100% - 50px)';
+        } else {
+            // Sinon affiche l'icone de la lune
+            toggle.checked = false;
+            sun.style.display = 'none';
+            moon.style.display = 'block';
+            selector.style.left = '10px';
+        }
+
+    }
+
+    toggle.addEventListener('click', function () {
 
         const swipe = document.getElementById('swipe');
-        const body = document.body;
 
         swipe.classList.add('current');
 
@@ -29,8 +56,10 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(() => {
                 // Inverse les couleurs en changeant la couleur des variables CSS
                 root.style.setProperty('--main-color', 'white');
-                root.style.setProperty('--second-color', 'black');
-
+                root.style.setProperty('--second-color', '#0c0c0c');
+                // on stocke les couleurs dans le localStorage
+                localStorage.setItem('--main-color', 'white');
+                localStorage.setItem('--second-color', '#0c0c0c');
 
                 swipe.classList.remove('current');
             }, 600);
@@ -44,9 +73,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
             setTimeout(() => {
                 // Inverse les couleurs en changeant la couleur des variables CSS
-                root.style.setProperty('--main-color', 'black');
+                root.style.setProperty('--main-color', '#0c0c0c');
                 root.style.setProperty('--second-color', 'white');
-
+                // on stocke les couleurs dans le localStorage
+                localStorage.setItem('--main-color', '#0c0c0c');
+                localStorage.setItem('--second-color', 'white');
 
                 swipe.classList.remove('current');
 
@@ -54,18 +85,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-// Script du bouton me contacter de la page d'accueil ======================================================================
+    // Script du bouton me contacter de la page d'accueil ======================================================================
     const contactLink = document.querySelector('.buttons-contact');
     const arrowIcon = contactLink.querySelector('.fa-arrow-right');
 
-    contactLink.addEventListener('click', function (event) {          
+    contactLink.addEventListener('click', function (event) {
         event.preventDefault(); // Empêche la redirection immédiate
         arrowIcon.style.transform = 'translateX(-200px)'; // Animation vers la gauche
 
 
         setTimeout(() => {
-            window.location.href = contactLink.getAttribute('href');
-            arrowIcon.style.transform = 'translateX(0px)';  // Redirection après animation
+            window.location.href = contactLink.getAttribute('href'); // Redirection vers la page de contact
+            arrowIcon.style.transform = 'translateX(0px)';  // la fleche revient à sa position initiale
         }, 300); // Temps pour l'animation (300ms)
     });
 
