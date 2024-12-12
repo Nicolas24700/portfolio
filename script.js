ec7e8a5786044c89d5b9f1cbbeb147d51dffb209
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2024-12-12 20:23:29 +0100

    page projet
    Ajout d'un json avec mes projets
    
    Ajout d'une animation d'inclinaison sur les divs projet

--- script.js
+++ script.js
@@ -1,4 +1,95 @@
 document.addEventListener('DOMContentLoaded', () => {
+    fetch('projet.json').then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            updateProjectCount(data)
+            generation(data);
+            effectinclinaison();
+        });
+    });
+
+    //  Script pour mettre à jour le nombre de projets affichés ===================================================
+    function updateProjectCount(data) {
+        const projectCount = data.length;
+        document.getElementById('numberprojet').textContent = projectCount;
+    }
+
+    // Fonction qui génère les éléments HTML à partir des données JSON========================================
+    function generation(data) {
+        data.forEach(function (projet) {
+            var languages = projet["languages"]
+                // Transforme chaque langue en une chaîne HTML avec une div et un paragraphe
+                .map(lang => "<div class='tags'><p>" + lang + "</p></div>")
+                // Joint toutes les chaînes HTML en une seule chaîne
+                .join('');
+            const projectHTML = `
+            <div class="projet-container">
+            <h2>${projet["titre"]}</h2>
+            <div class="container">
+                <img src="${projet["img-projet"]}" alt="" class="image-projet">
+                <div class="text-section">
+                    <p>Date : ${projet["date"]}</p>
+                    <div class="language-tag">
+                        ${languages}
+                    </div>
+                    <p>
+                        ${projet["description"]}
+                    </p>
+                    <p><strong>Compétences développées :</strong> ${projet["competenceDev"]}</p>
+                    <p><strong>Réalisation : </strong>${projet["realisation"]}</p>
+                    <p><strong>Contexte :</strong> ${projet["contexte"]}</p>
+                    <div class="projet-linkDIV">
+                        <a href="${projet["github-link"]}" target="_blank" class="projet-link" aria-label="lien github du projet"><i class="fa-brands fa-github"></i></a>
+                        <a href="${projet["lien"]}" target="_blank" class="projet-link">Lien du projet <i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+
+            const projectContainer = document.querySelector('.allprojets');
+
+            // Si le conteneur de projet existe, on ajoute le HTML du projet sinon on fait rien
+            if (projectContainer) {
+                projectContainer.innerHTML += projectHTML;
+            }
+        });
+    }
+
+    // SCRIPT POUR L'inclinaison de la div projet ========================================================================
+    function effectinclinaison() {
+        const projetdiv = document.querySelectorAll('.projet-container');
+    
+        projetdiv.forEach(div => {
+            div.addEventListener('mousemove', (e) => {
+                // Récupère les coordonnées de la souris par rapport à la div
+                const rect = div.getBoundingClientRect();
+                const mouseX = e.clientX - rect.left;
+                const mouseY = e.clientY - rect.top;
+    
+                // Calcule le centre de la div
+                const centerX = rect.width / 2;
+                const centerY = rect.height / 2;
+    
+                // Calcule la différence entre la position de la souris et le centre de la div
+                const deltaX = (mouseX - centerX) / centerX;
+                const deltaY = (mouseY - centerY) / centerY;
+    
+                const maxTilt = 5; // L'angle maximum d'inclinaison
+
+                // Calcule l'angle d'inclinaison en fonction de la position de la souris
+                const tiltX = deltaY * maxTilt;
+                const tiltY = -deltaX * maxTilt;
+    
+                // Applique la transformation CSS à la div
+                div.style.transform = `perspective(500px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+            });
+             // Réinitialise la transformation lorsque la souris quitte la div
+            div.addEventListener('mouseleave', () => {
+                div.style.transform = 'perspective(0px) rotateX(0deg) rotateY(0deg)';
+            });
+        });
+        }
 
     // Script de la barre de navigation =============================================================================
     const hamburger = document.getElementById('hamburger');
@@ -87,17 +178,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Script du bouton a propos de la page d'accueil ======================================================================
     const contactLink = document.querySelector('.buttons-apropos');
-    const arrowIcon = contactLink.querySelector('.fa-arrow-right');
 
-    contactLink.addEventListener('click', function (event) {
-        event.preventDefault(); // Empêche la redirection immédiate
-        arrowIcon.style.transform = 'translateX(-200px)'; // Animation vers la gauche
+    // Si le bouton existe
+    if (contactLink) {
+        const arrowIcon = contactLink.querySelector('.fa-arrow-right');
+        // Si l'icone existe dans le bouton on fait l'animation
+        if (arrowIcon) {
+            contactLink.addEventListener('click', function (event) {
+                event.preventDefault(); // Empêche la redirection immédiate
+                arrowIcon.style.transform = 'translateX(-200px)'; // Animation vers la gauche
 
 
-        setTimeout(() => {
-            window.location.href = contactLink.getAttribute('href'); // Redirection vers la page de contact
-            arrowIcon.style.transform = 'translateX(0px)';  // la fleche revient à sa position initiale
-        }, 200); // Temps pour l'animation (300ms)
-    });
+                setTimeout(() => {
+                    window.location.href = contactLink.getAttribute('href'); // Redirection vers la page de contact
+                    arrowIcon.style.transform = 'translateX(0px)';  // la fleche revient à sa position initiale
+                }, 200); // Temps pour l'animation (300ms)
+            });
+
+
+        }
+    }
 
 });
